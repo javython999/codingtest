50822fc219330a0e7b81586576a46a3cb8e89875
javython999 <infinity2544@gmail.com>
2026-04-09 20:20:41 +0900

    dp

--- src/test/java/com/errday/codingtest/dp/MakeIt1.java
+++ src/test/java/com/errday/codingtest/dp/MakeIt1.java
@@ -2,84 +2,58 @@ package com.errday.codingtest.dp;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MakeIt1 {
 
-
     @Test
-    void solution() {
-        int n = 10;
-        int answer = 4;
-        int[] memo  = new int[n + 1];
-
-        for (int i = 1; i < memo.length; i++) {
-            List<Integer> temp = new ArrayList<>();
-
-            if (i % 3 == 0) {
-                temp.add(memo[i / 3] + 1);
-            }
-
-            if (i % 2 == 0) {
-                temp.add(memo[i / 2] + 1);
-            }
-
-            temp.add(memo[i - 1] + 1);
-
-            memo[i] = temp.stream()
-                            .min(Integer::compare)
-                            .get();
-
-        }
-
-        assertThat(memo[n]).isEqualTo(answer);
+    void case1() {
+        int x = 2;
+        int answer = 1;
+        assertThat(solution(x)).isEqualTo(answer);
     }
 
     @Test
-    void solution2() {
-        int n = 26;
+    void case2() {
+        int x = 10;
         int answer = 3;
+        assertThat(solution(x)).isEqualTo(answer);
+    }
 
-        // n이 5로 나누어 떨어지면 5로 나눈다.
-        // n이 3으로 나누어 떨어지면 3으로 나눈다.
-        // n이 2로 나누어 떨어지면 2로 나눈다.
-        // n에서 1을 뺀다.
+    @Test
+    void case3() {
+        int x = 45;
+        int answer = 5;
+        assertThat(solution(x)).isEqualTo(answer);
+    }
+
+    @Test
+    void case4() {
+        int x = 7;
+        int answer = 3;
+        assertThat(solution(x)).isEqualTo(answer);
+    }
+
+    private int solution(int n) {
 
         int[] memo = new int[n + 1];
-        memo[1] = 1;
-        memo[2] = 1;
-        memo[3] = 1;
-        memo[5] = 1;
+        memo[0] = 0;
+        memo[1] = 0;
 
-        for (int i = 1; i < memo.length; i++) {
-            if (memo[i] != 0) {
-                continue;
-            }
+        for (int i = 2; i <= n; i++) {
 
-            List<Integer> temp = new ArrayList<>();
+            memo[i] = memo[i - 1] + 1;
 
-            if (i % 5 == 0) {
-                temp.add(memo[i / 5] + 1);
+            if (i % 2 == 0) {
+                memo[i] = Math.min(memo[i], memo[i / 2] + 1);
             }
 
             if (i % 3 == 0) {
-                temp.add(memo[i / 3] + 1);
+                memo[i] = Math.min(memo[i], memo[i / 3] + 1);
             }
 
-            if (i % 2 == 0) {
-                temp.add(memo[i / 2] + 1);
-            }
-
-            temp.add(memo[i - 1] + 1);
-
-            memo[i] = temp.stream()
-                    .min(Integer::compare)
-                    .get();
         }
 
-        assertThat(memo[n]).isEqualTo(answer);
+        return memo[n];
     }
 }
